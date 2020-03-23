c515217bb811e19d15a603bc17431eebf6817dff
chenyuru <1244535094@qq.com>
2020-03-23 16:24:53 +0800

    login

---
+++ view/src/components/UserInfo.vue
@@ -0,0 +1,52 @@
+<template>
+<div>
+  <el-container style="width: 1000px; border: 1px solid rgba(211,211,211,0.09);margin-left: 200px;">
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </el-header>
+
+
+      <el-main>
+        <el-tabs :tab-position="tabPosition" style="height: 200px;" head="个人中心">
+          <el-tab-pane label="用户管理">用户管理</el-tab-pane>
+          <el-tab-pane label="配置管理">配置管理</el-tab-pane>
+          <el-tab-pane label="角色管理">角色管理</el-tab-pane>
+          <el-tab-pane label="定时任务补偿">定时任务补偿</el-tab-pane>
+        </el-tabs>
+      </el-main>
+  </el-container>
+
+</div>
+</template>
+
+<script>
+    export default {
+        name: "UserInfo",
+      data() {
+        return {
+          tabPosition:'left'
+        }
+      }
+    }
+
+</script>
+
+<style scoped>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #333;
+  }
+</style>
